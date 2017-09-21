a73fbb98c0ef280b562f58e464dae9da9b3d4c33
bingjie.huang <bingjie.huang@shuyun.com>
2017-09-21 18:46:13 +0800

    remove dist

--- dist/Store.d.ts
+++
@@ -1,5 +0,0 @@
-import { AbstractActor } from "js-actor";
-export declare class Store<S> extends AbstractActor {
-    state: S;
-    setState: (state: Partial<S>, callback?: () => void) => void;
-}
--- dist/connect/react/connect.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import * as React from "react";
-import { Store } from "../../Store";
-export declare function connect<S extends object>(store: new () => Store<S>): <P>(component: React.ComponentClass<P>) => any;
