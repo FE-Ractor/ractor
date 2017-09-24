4903dea54953f0537fbc434d5923ed956380a204
bingjie.huang <bingjie.huang@shuyun.com>
2017-09-24 13:02:08 +0800

    更改了 monkeypatch 部分的 setState，改为类似  redux 的 listener

--- src/Store.ts
+++ src/Store.ts
@@ -2,5 +2,23 @@ import { AbstractActor, Receive } from "js-actor"
 
 export class Store<S> extends AbstractActor {
 	public state = {} as S
-	public setState: (state: Partial<S>, callback?: () => void) => void
+	public listeners: Listener<S>[] = []
+
+	public subscribe(listener: Listener<S>) {
+		this.listeners.push(listener)
+		return () => {
+			const index = this.listeners.indexOf(listener)
+			this.listeners.splice(index, 1)
+		}
+	}
+
+	public setState(nextState: Partial<S>, callback?: () => void) {
+		Object.assign(this.state, nextState)
+		for (let listener of this.listeners) {
+			listener(this.state)
+		}
+	}
 }
+
+
+export type Listener<S> = (state: S, callback?: () => void) => void
--- src/connect/react/connect.ts
+++ src/connect/react/connect.ts
@@ -8,20 +8,26 @@ export function connect<S extends object>(store: new () => Store<S>) {
 		return class ConnectedComponent extends React.Component<P, S> {
 			private actorRef: ActorRef
 			private actor: Store<S>
+			private unsubscribe: () => void
+
 			constructor() {
 				super()
 				this.actor = new store()
 				this.state = this.actor.state
-				this.actor.setState = (state: Pick<S, keyof S>, callback: () => void) => {
-					this.actor.state = Object.assign(this.actor.state, state)
-					this.setState(state, callback)
-				}
 			}
 			public componentWillUnmount() {
+				// 从系统中移除并停止我们的 store
 				system.stop(this.actorRef)
+				// 取消监听 store, 这个取消操作是否可以省略？
+				this.unsubscribe()
 			}
 			public componentDidMount() {
+				// 为系统安装我们的 store
 				this.actorRef = system.actorOf(this.actor, "__store__")
+				// 订阅已经启动的 store，监听 store 的状态变化
+				this.unsubscribe = this.actor.subscribe((state, callback) => {
+					this.setState(state, callback)
+				})
 			}
 			public render() {
 				return React.createElement(component, Object.assign({}, this.props, this.state))
