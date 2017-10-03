7b1c7de1307e482f2c83f9c8a65f00f95ba87435
bingjie.huang <bingjie.huang@shuyun.com>
2017-10-03 15:03:24 +0800

    subscribe 都返回 subscription

--- src/Store.ts
+++ src/Store.ts
@@ -1,10 +1,10 @@
 import { AbstractActor, Receive } from "js-actor"
 
-export class Store<S> extends AbstractActor {
+export abstract class Store<S> extends AbstractActor {
 	public state = {} as S
 	public listeners: Array<Listener<S> | Observer<S>> = []
+	public abstract createReceive(): Receive
 
-	
 	/**
 	 * 新增了对 rxjs 的 Observable 的支持，后续考虑支持 https://github.com/tc39/proposal-observable
 	 * 灵感来源：https://github.com/reactjs/redux/blob/master/src/createStore.js#L209
@@ -12,29 +12,24 @@ export class Store<S> extends AbstractActor {
 	 * listener can be function or observer of rxjs
 	 * @param listener 
 	 */
-	public subscribe(listener: (Listener<S> | Observer<S>)) {
+	public subscribe(listener: (Listener<S> | Observer<S>)): Subscription {
 
 		if (typeof listener === "object" && listener.next) {
 			this.listeners.push(listener)
 			listener.next(this.state)
-			return {
-				unsubscribe: () => {
-					const index = this.listeners.indexOf(listener)
-					this.listeners.splice(index, 1)
-				}
-			}
-		}
-
-		if (typeof listener === "function") {
+		} else if (typeof listener === "function") {
 			this.listeners.push(listener)
 			listener(this.state)
-			return () => {
+		} else {
+			throw TypeError("expected the listener to be an function or observer.")
+		}
+
+		return {
+			unsubscribe: () => {
 				const index = this.listeners.indexOf(listener)
 				this.listeners.splice(index, 1)
 			}
 		}
-
-		throw TypeError("expected the listener to be an function or observer.")
 	}
 
 	/**
@@ -59,4 +54,7 @@ export class Store<S> extends AbstractActor {
 export type Listener<S> = (state: S) => void
 export type Observer<S> = {
 	next: (state: S) => void
+}
+export type Subscription = {
+	unsubscribe: () => void
 }
