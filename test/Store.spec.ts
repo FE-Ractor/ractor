4e6b0215f72807a0ca8b1723623a57094b2fa7ff
bingjie.huang <bingjie.huang@shuyun.com>
2017-10-09 15:23:17 +0800

    新增测试用例

--- test/Store.spec.ts
+++ test/Store.spec.ts
@@ -2,10 +2,16 @@ import { test } from "ava"
 import { Subject } from "rxjs"
 import { Store } from "../src/Store"
 
+class TestStore<S> extends Store<S> {
+  public createReceive() {
+    return this.receiveBuilder().build()
+  }
+}
+
 test("store should support subscribe to observer of rxjs", t => {
   const subject = new Subject
-  const store: Store<{ value: 1 }> = new Store
+  const store = new TestStore<{ value: 1 }>()
   store.subscribe(subject)
   subject.subscribe((state: { value: number }) => t.is(state.value, 1))
-  store.setState({value: 1})
+  store.setState({ value: 1 })
 })
