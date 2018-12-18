6606c0f7f566afa1b5ba732079c39b7ee2af0774
黄斌杰 <akira.binjie@gmail.com>
2018-12-18 19:07:44 +0800

    export createStore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Store, Subscription } from "./Store"
 export { System } from "./System"
 export { ActorReceiveBuilder as ReceiveBuilder } from "js-actor"
+export { createStore } from "./createStore"
